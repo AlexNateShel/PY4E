91ca19ce70299d5bf2c9334d4ebc23a381646e40
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2024-02-27 23:36:32 -0500

    Create Exercise 12.1

---
+++ Python For Everybody (Book)/Chapter 12/exercise_12.1.py
@@ -0,0 +1,23 @@
+# Python For Everybody Chapter 12 Exercise 1
+
+# Change the socket program socket1.py to prompt the user for the URL so it can read any web page.
+# You can use the split('/') to break the URL into its component parts so you can extract the host name for the socket connect call.
+# Add error checking using try and except to handle the condition where the user enters an improperly formatted or non-existent URL.
+
+import socket
+
+web = input('Enter a website: ')
+host = web.split('/')
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((host[2], 80))
+cmd = 'GET ' + web + ' HTTP/1.0\r\n\r\n'
+cmd = cmd.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(500)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+mysock.close()
