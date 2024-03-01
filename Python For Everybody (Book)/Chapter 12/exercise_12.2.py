97706d88a8a2be549a3e72949ab7bc833818e129
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2024-03-01 00:00:23 -0500

    Create Exercise 12.2

---
+++ Python For Everybody (Book)/Chapter 12/exercise_12.2.py
@@ -0,0 +1,6 @@
+# Python For Everybody Chapter 12 Exercise 2
+
+# Change the socket program so that it counts the number of characters it has received and stops displaying any text after it has shown 3000 characters.
+# The program should retrieve the entire document and count the total number of characters and display the count of the number os characters at the end of the document.
+
+
